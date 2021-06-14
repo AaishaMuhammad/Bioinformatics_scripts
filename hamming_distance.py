3d0aa4b687e187aa824e7e807728df81be357100
AaishaM <aaisha@pearlcoral.com>
2021-06-14 10:00:39 +0200

    Hamming Distance Calculator

---
+++ hamming_distance.py
@@ -0,0 +1,17 @@
+'''
+A script that calculates the total number of mismatches between two strings of identical length.
+
+'''
+
+def hamming(genome_a, genome_b):
+    h_dis = 0
+    for a, b in zip(genome_a, genome_b):
+        if a != b:
+            h_dis +=1
+    return h_dis
+
+
+# Testing on provided debug dataset. Substitute for the .txt data file.
+
+
+print(hamming("AGAAACAGACCGCTATGTTCAACGATTTGTTTTATCTCGTCACCGGGATATTGCGGCCACTCATCGGTCAGTTGATTACGCAGGGCGTAAATCGCCAGAATCAGGCTG", "AGAAACCCACCGCTAAAAACAACGATTTGCGTAGTCAGGTCACCGGGATATTGCGGCCACTAAGGCCTTGGATGATTACGCAGAACGTATTGACCCAGAATCAGGCTC"))
